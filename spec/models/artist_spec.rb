e5c3ff83c037506e8f4e3650a7735f11d8e8efd1
Prajna Zhang <nihilism.z@gmail.com>
2011-07-23 21:25:59 +0800

    Add test for artist

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
 describe Artist do
-  it "should be wrong if name is empty"
+  it "should be wrong if name is empty" do
+    Artist.new.should have(1).error_on(:name) 
+  end
   
-  it "should be wrong if email is empty"
+  it "should be wrong if email is empty" do
+    Artist.new.should have(2).error_on(:email)
+  end
+  
+  it "should be wrong if email is invalid" do
+    Artist.new(:name => "alice", :email => "dkfj.com").should have(1).error_on(:email)
+  end
+  
+  it "should be right" do
+    Artist.new(:name => "alice", :email => "fake@gmail.com").should have(0).error_on(:email)
+  end
 end
