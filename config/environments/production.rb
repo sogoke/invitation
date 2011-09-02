a5d11a372a2b80a58af15baf1f86b13effa51e6e
Prajna Zhang <nihilism.z@gmail.com>
2011-09-02 21:09:11 +0800

    Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Invitation::Application.configure do
 
   # Code is not reloaded between requests
   config.cache_classes = true
+  config.assets.digest = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
@@ -19,7 +20,7 @@ Invitation::Application.configure do
 
   # Specifies the header that your server uses for sending files
   # (comment out if your front-end server doesn't support this)
-  config.action_dispatch.x_sendfile_header = "X-Sendfile" # Use 'X-Accel-Redirect' for nginx
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # Use 'X-Accel-Redirect' for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
