9e4bb75669470c2da131bc6e42b79d9285f8c9f4
kuno <neokuno@gmail.com>
2011-09-02 18:11:47 +0800

    make name small

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,3 +1,5 @@
+var SELFINTRO_LENGTH = 140;
+
 $('#applyButton').click(function() {
     $('.popover').fadeIn(100);
 });
@@ -17,12 +19,12 @@ $('.popover div#request div.detail form.request fieldset textarea.selfIntro').ke
     $('.popover div#request div.detail form.request fieldset strong.selfIntroError').hide();
     var left, exceed, inputs = $(this).val().length;
 
-    if (inputs === SELFINTRO_LENGTH) {
+    if (inputs == SELFINTRO_LENGTH) {
       $('span.selfIntroCounter').html('<em class="full">已经到了上限，<br />可以提交了!</em>').show();
     } else if (inputs > SELFINTRO_LENGTH) {
       exceed = inputs - SELFINTRO_LENGTH;
       $('span.selfIntroCounter').html('最多140个字，<br />已超出<em class="exceed">' + exceed + '</em>个字').show();
-    } else {
+    } else if (inputs < SELFINTRO_LENGTH) {
       left = SELFINTRO_LENGTH - inputs;
       $('span.selfIntroCounter').html('最多140个字，<br />还剩<em class="left">' + left + '</em>个字').show();
     }
